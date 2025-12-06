8ef14e56b942d782617cf647f13d2c817e6cd181
marcotxt2 <marcotuliotutim@gmail.com>
2025-12-06 15:18:00 -0300

    Melhorando legibilidade do código | Criando classe e definindo função de filtrar tabelas reais.

--- main.py
+++ main.py
@@ -2,46 +2,66 @@ import tabula
 import pandas as pd
 import zipfile
 
-Arquivo_Zip_Nome = "Teste_Marco.zip"
-Arquivo_Csv = "Anexo_I.csv"
-Arquivo_Pdf = "anexoI/Anexo_I.pdf"
+ARQUIVO_ZIP_NOME = "Teste_Marco.zip"
+ARQUIVO_CSV = "Anexo_I.csv"
+ARQUIVO_PDF = "anexoI/Anexo_I.pdf"
 Minimo_Linhas = 10 #Defini 10 pois identifiquei que a menor tabela do pdf tem 10 linhas apenas.
 
-#Com a biblioteca tabula, o programa identifica todas as tabelas do PDF. (lattice para identificar as linhas com bordas definidas)
-Lista_Tabelas = tabula.read_pdf(Arquivo_Pdf, pages= "all",encoding='utf-8', lattice = True)
+def filtrar_tabelas_reais(minimo_linhas: int, lista_tabelas: list):
+    """
+    Filtra as tabelas que possuem um número mínimo de linhas.
+    :param minimo_linhas: Pega o valor minimo de linhas para ser definido real.
+    :param lista_tabelas: Pega o array das tabelas fornecidos pela tabula.
+    :return: Retorna a lista de tabelas reais (válidas).
+    """
+    Tabelas_Reais = []
+    for tabela in lista_tabelas:
+        if len(tabela) >= minimo_linhas:
+            Tabelas_Reais.append(tabela)
+    return Tabelas_Reais
 
-#For loop para analisar se a tabela tem menor ou igual a 10 linhas (garantindo assim que seja uma tabela válida do PDF)
-Tabelas_Reais = []
-for tabela in Lista_Tabelas:
-    if len(tabela) >= Minimo_Linhas:
-        Tabelas_Reais.append(tabela)
-print("Tabelas a serem extraidas: ", len(Tabelas_Reais))
-
-if not Tabelas_Reais:
-    print ("Nenhuma tabela válida encontrada.")
-
-if len(Tabelas_Reais) > 0:
-    #Empilha as tabelas
-    Tabela_Completa = pd.concat(Tabelas_Reais, ignore_index=True) #ignore_index deixa de resetar o index para cada linha da tabela.
-
-    #Identifica a coluna "OD" e "AMB", faz o replace de cada para o inserido no rodapé das páginas.
-    #Obs.: Utilizado regex para não ter erro do tabula ter identificado os elementos com espaço vazio por exemplo " OD"...
-    if "OD" in Tabela_Completa.columns:
-        Tabela_Completa["OD"] = Tabela_Completa["OD"].str.replace("OD","Seg. Odontológica", regex = True)
-    if "AMB" in Tabela_Completa.columns:
-        Tabela_Completa["AMB"] = Tabela_Completa["AMB"].str.replace("AMB","Seg. Ambulatorial", regex = True)
-
-    print("Trocando os elementos OD e AMB por Seg. Odontológica e Seg. Ambulatorial respectivamente")
-
-    #Transforma para CSV a tabela completa.
+def main():
+    #Com a biblioteca tabula, o programa identifica todas as tabelas do PDF. (lattice para identificar as linhas com bordas definidas)
     try:
-        Tabela_Completa.to_csv(Arquivo_Csv, index=False, encoding='utf-8-sig')
-        print("CSV Criado: ", Arquivo_Csv)
-
-        #Pega o arquivo "Anexo_I.csv" e guarda dentro do arquivo zip "AnexoI_CSV.zip".
-        with zipfile.ZipFile(Arquivo_Zip_Nome, 'w') as Arquivo_Zip:
-            Arquivo_Zip.write(Arquivo_Csv)
-        print("ZIP criado: ", Arquivo_Zip_Nome)
-
+        print("Iniciando identificação de tabelas no PDF...")
+        Lista_Tabelas = tabula.read_pdf(ARQUIVO_PDF, pages="all", encoding='utf-8', lattice = True)
     except Exception as e:
-        print("Erro ao salvar arquivos: ", e)
+        print("Erro ao ler arquivos: ", e)
+        return
+
+    #For loop para analisar se a tabela tem menor ou igual a 10 linhas (garantindo assim que seja uma tabela válida do PDF)
+    Tabelas_Reais = filtrar_tabelas_reais(Minimo_Linhas, Lista_Tabelas)
+    Qtd_Tabelas_Reais = len(Tabelas_Reais)
+
+    if not Qtd_Tabelas_Reais:
+        print ("Nenhuma tabela válida encontrada.")
+        return
+
+    print(f"{Qtd_Tabelas_Reais} Tabelas encontradas...")
+
+    if Qtd_Tabelas_Reais > 0:
+        #Empilha as tabelas
+        Tabela_Completa = pd.concat(Tabelas_Reais, ignore_index=True) #ignore_index deixa de resetar o index para cada linha da tabela.
+
+        #Identifica a coluna "OD" e "AMB", faz o replace de cada para o inserido no rodapé das páginas.
+        #Obs.: Utilizado regex para não ter erro do tabula ter identificado os elementos com espaço vazio por exemplo " OD"...
+        if "OD" in Tabela_Completa.columns:
+            Tabela_Completa["OD"] = Tabela_Completa["OD"].str.replace("OD","Seg. Odontológica", regex = True)
+        if "AMB" in Tabela_Completa.columns:
+            Tabela_Completa["AMB"] = Tabela_Completa["AMB"].str.replace("AMB","Seg. Ambulatorial", regex = True)
+
+        print("Trocando os elementos OD e AMB por Seg. Odontológica e Seg. Ambulatorial respectivamente")
+
+        #Transforma para CSV a tabela completa.
+        try:
+            Tabela_Completa.to_csv(ARQUIVO_CSV, index=False, encoding='utf-8-sig')
+            print("CSV Criado: ", ARQUIVO_CSV)
+
+            #Pega o arquivo "Anexo_I.csv" e guarda dentro do arquivo zip "AnexoI_CSV.zip".
+            with zipfile.ZipFile(ARQUIVO_ZIP_NOME, 'w') as Arquivo_Zip:
+                Arquivo_Zip.write(ARQUIVO_CSV)
+            print("ZIP Criado: ", ARQUIVO_ZIP_NOME)
+
+        except Exception as e:
+            print("Erro ao salvar arquivos: ", e)
+main()
